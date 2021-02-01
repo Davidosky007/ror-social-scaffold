63be941f8a3be8a0b3cbf9ea653b3ce26c3b9b59
David Bassey <davidosky1@gmail.com>
2021-02-01 15:48:08 +0100

    installs rspec, creates a friends method in the User model and writes test in models/user_spec.rb

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  let(:user1) { User.create(id: 91, name: 'user1', email: 'user1@gmail.com', password: '123456') }
+  let(:user2) { User.create(id: 92, name: 'user2', email: 'user2@gmail.com', password: '123456') }
+  let(:user3) { User.create(id: 93, name: 'user3', email: 'user3@gmail.com', password: '123456') }
+  describe 'friends' do
+    it 'returns friends of a given user' do
+      Friendship.create(user_id: 91, friend_id: 92, status: true)
+      Friendship.create(user_id: 91, friend_id: 93, status: true)
+      puts 'output'
+      puts user1.friends
+      expect(user1.friends).to eq(%w[user1 user2])
+    end
+  end
+end
