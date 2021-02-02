89b1c757b8f6ef83467bf179b5ade4178749f1f2
David Bassey <davidosky1@gmail.com>
2021-02-02 12:02:59 +0100

    updates shoulda matchers gem, writes test for comments model, posts model and likes model

---
+++ spec/comments_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+require 'shoulda/matchers'
+
+RSpec.describe Comment do
+  let(:user) { User.create(name: 'Example User', email: 'abc@example.com', password: 'abc') }
+  let(:post) { Post.create(content: 'This is a test post', user_id: user.id) }
+  let(:subject) do
+    described_class.new(
+      post_id: post.id,
+      user_id: user.id,
+      content: 'This is a test comment'
+    )
+  end
+
+  describe 'validations' do
+
+    it 'The content of the comment length < 200' do
+      subject.content = '0' * 205
+      expect(subject).not_to be_valid
+    end
+  end
+
+  describe 'Associations', type: :model do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:post) }
+  end
+end
