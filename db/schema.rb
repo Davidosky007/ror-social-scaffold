3d36e522cf70ed81a795e8e63a23863aba431710
David Bassey <davidosky1@gmail.com>
2021-02-05 03:05:13 +0100

    creates new migration file to fixs database errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_03_104016) do
+ActiveRecord::Schema.define(version: 2021_02_05_014704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
