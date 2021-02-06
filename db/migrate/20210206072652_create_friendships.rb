db2263356622038697028dd88004b5596ae90a1a
David Bassey <davidosky1@gmail.com>
2021-02-06 08:33:51 +0100

    modifies schema.rb file

--- db/migrate/20210206072652_create_friendships.rb
+++ db/migrate/20210206072652_create_friendships.rb
@@ -2,10 +2,11 @@ class CreateFriendships < ActiveRecord::Migration[5.2]
   def change
     create_table :friendships do |t|
       t.references :user, index: true, foreign_key: true
-      t.references :friend, foreign_key: { to_table: :users }, index: true
+      t.references :friend, index: true
       t.boolean :confirmed
 
       t.timestamps null: false
     end
+    add_foreign_key :friendships, :users, column: :friend_id
   end
 end
